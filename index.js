6e2bd02318866de06433de9dc2c97a181094d72f
Khan Suhel Ahmed <ksuhel80@gmail.com>
2024-01-11 14:58:17 +0530

    added path
    added path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors")
+const path = require("path")
 
 //dotenv conig
 dotenv.config();
